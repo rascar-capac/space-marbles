2ba2a9d21534ee2cfccd5e6a79af66d8870d08f8
rascar-capac <to.constant@gmail.com>
2020-06-02 14:06:17 +0200

    Enhance planet names generation
    Adapt to the fake news system (with a text file)

--- Assets/Scripts/PlanetGenerator.cs
+++ Assets/Scripts/PlanetGenerator.cs
@@ -7,8 +7,8 @@ public class PlanetGenerator : MonoBehaviour
 {
     [SerializeField] private IngredientInitializer ingredient = null;
     [SerializeField] private PlanetInitializer planetPrefab = null;
-    [SerializeField] private NameTemplatesData nameTemplates = null;
     [SerializeField] private int segmentsCount = 30;
+    [SerializeField] private TextAsset nameData = null;
 
     public Dictionary<IngredientData.IngredientType, IngredientInitializer> Ingredients { get; set; }
     public bool CanGeneratePlanet { get; set; }
@@ -24,6 +24,34 @@ public class PlanetGenerator : MonoBehaviour
         GetComponent<CircleCollider2D>().radius = influenceZone;
         this.canvas = canvas;
         this.mainCamera = mainCamera;
+    }
+
+    public void SpawnPlanet()
+    {
+        string planetName = ComputeRandomName();
+        Texture2D surface = Ingredients[IngredientData.IngredientType.SOLID].Data.Surface;
+        Texture2D pattern = Ingredients[IngredientData.IngredientType.SOLID].Data.Pattern;
+        Color[] colors = Ingredients[IngredientData.IngredientType.LIQUID].Data.Colors;
+        Texture2D extra = Ingredients[IngredientData.IngredientType.GASEOUS].Data.Extra;
+        float averageMass = 0;
+        float averageDrag = 0;
+        float averageAngularDrag = 0;
+        foreach(IngredientInitializer ingredient in Ingredients.Values)
+        {
+            Rigidbody2D rb = ingredient.GetComponent<Rigidbody2D>();
+            averageMass += rb.mass;
+            averageDrag += rb.drag;
+            averageAngularDrag += rb.angularDrag;
+        }
+        averageMass /= Ingredients.Count;
+        averageDrag /= Ingredients.Count;
+        averageAngularDrag /= Ingredients.Count;
+        PlanetData data = new PlanetData(planetName, surface, pattern, colors, extra,
+                averageMass, averageDrag, averageAngularDrag);
+
+        PlanetInitializer generatedPlanet = Instantiate(planetPrefab, spawnPosition, Quaternion.identity);
+        generatedPlanet.InitData(data);
+        generatedPlanet.Init(canvas, mainCamera);
     }
 
     private void Awake()
@@ -127,224 +155,11 @@ public class PlanetGenerator : MonoBehaviour
         }
     }
 
-    public void SpawnPlanet()
-    {
-        string planetName = ComputeRandomName();
-        Texture2D surface = Ingredients[IngredientData.IngredientType.SOLID].Data.Surface;
-        Texture2D pattern = Ingredients[IngredientData.IngredientType.SOLID].Data.Pattern;
-        Color[] colors = Ingredients[IngredientData.IngredientType.LIQUID].Data.Colors;
-        Texture2D extra = Ingredients[IngredientData.IngredientType.GASEOUS].Data.Extra;
-        float averageMass = 0;
-        float averageDrag = 0;
-        float averageAngularDrag = 0;
-        foreach(IngredientInitializer ingredient in Ingredients.Values)
-        {
-            Rigidbody2D rb = ingredient.GetComponent<Rigidbody2D>();
-            averageMass += rb.mass;
-            averageDrag += rb.drag;
-            averageAngularDrag += rb.angularDrag;
-        }
-        averageMass /= Ingredients.Count;
-        averageDrag /= Ingredients.Count;
-        averageAngularDrag /= Ingredients.Count;
-        PlanetData data = new PlanetData(planetName, surface, pattern, colors, extra,
-                averageMass, averageDrag, averageAngularDrag);
-
-        PlanetInitializer generatedPlanet = Instantiate(planetPrefab, spawnPosition, Quaternion.identity);
-        generatedPlanet.InitData(data);
-        generatedPlanet.Init(canvas, mainCamera);
-    }
-
     private string ComputeRandomName()
     {
-        string template = nameTemplates.Templates[Random.Range(0, nameTemplates.Templates.Count)];
-        List<IngredientData.Noun> nouns = new List<IngredientData.Noun>();
-        nouns.Add(null);
-
-        while(template.Contains("§"))
-        {
-            string element = GetTextBetweenTags(template, "§", "§");
-
-            int index = 0;
-            if(element.Contains("#"))
-            {
-                index = int.Parse(element.Substring(element.Length - 1));
-                element = element.Remove(element.Length - 2);
-            }
-            if(element.Contains(","))
-            {
-                element = GetRandomChoice(element);
-            }
-
-            IngredientData.Noun noun = null;
-            switch(element)
-            {
-                case "planet synonym" :
-                    noun = Ingredients[IngredientData.IngredientType.SOLID].Data.NamingElements.planetSynonym;
-                    break;
-                case "concept" :
-                    noun = Ingredients[IngredientData.IngredientType.SOLID].Data.NamingElements.concept;
-                    break;
-                case "first name" :
-                    noun = Ingredients[IngredientData.IngredientType.LIQUID].Data.NamingElements.firstName;
-                    break;
-                case "nickname" :
-                    noun = Ingredients[IngredientData.IngredientType.SOLID].Data.NamingElements.nickname;
-                    break;
-                case "matriculation" :
-                    noun = Ingredients[IngredientData.IngredientType.GASEOUS].Data.NamingElements.matriculation;
-                    break;
-            }
-
-            if(index != 0)
-            {
-                nouns.Insert(index, noun);
-            }
-
-            element = noun.word;
-            template = ReplaceTextBetweenTags(template, element, "§", "§");
-        }
-
-        while(template.Contains("+"))
-        {
-            string element = GetTextBetweenTags(template, "+", "+");
-
-            IngredientData.Qualifier qualifier = null;
-            int index = 0;
-            index = int.Parse(element.Substring(element.Length - 1).ToString());
-            element = element.Remove(element.Length - 2);
-            switch(element)
-            {
-                case "style adjective" :
-                    qualifier = Ingredients[IngredientData.IngredientType.GASEOUS].Data.NamingElements.styleAdjective;
-                    break;
-                case "personality adjective" :
-                    qualifier = Ingredients[IngredientData.IngredientType.LIQUID].Data.NamingElements.personalityAdjective;
-                    break;
-                case "color" :
-                    qualifier = Ingredients[IngredientData.IngredientType.LIQUID].Data.NamingElements.color;
-                    break;
-            }
-            if(index == 0)
-            {
-                element = qualifier.singularFeminine;
-            }
-            else
-            {
-                element = MatchQualifier(qualifier, nouns[index]);
-            }
-
-            template = ReplaceTextBetweenTags(template, element, "+", "+");
-        }
-
-        while(template.Contains("<"))
-        {
-            string article = GetTextBetweenTags(template, "<", ">");
-            int index = int.Parse(article.Substring(article.Length - 1));
-            template = ReplaceTextBetweenTags(template, MatchArticle(article, nouns[index]), "<", ">");
-        }
-
-        while(template.Contains("{"))
-        {
-            string article = template.Substring(template.IndexOf("{") + 4, template.IndexOf("}") - 1);
-            template = ReplaceTextBetweenTags(template, MatchPossessive(article), "{", "}");
-        }
-
-        return template.Substring(0, 1).ToUpper() + template.Substring(1);
-    }
-
-    private string GetTextBetweenTags(string context, string tag1, string tag2)
-    {
-        int startIndex = context.IndexOf(tag1) + 1;
-        int length = context.Substring(startIndex).IndexOf(tag2);
-        return context.Substring(startIndex, length);
-    }
-
-    private string ReplaceTextBetweenTags(string context, string replacement, string tag1, string tag2)
-    {
-        int startIndex = context.IndexOf(tag1);
-        int length = context.Substring(startIndex + 1).IndexOf(tag2) + 2;
-        return context.Replace(context.Substring(startIndex, length), replacement);
-    }
-
-    private string MatchQualifier(IngredientData.Qualifier qualifier, IngredientData.Noun noun)
-    {
-        if(qualifier.invariable != "")
-        {
-            return qualifier.invariable;
-        }
-
-        if(noun == null)
-        {
-            return qualifier.singularFeminine;
-        }
-
-        if(noun.genre == IngredientData.Genre.FEMININE)
-        {
-            if(noun.plurality == IngredientData.Plurality.SINGULAR)
-            {
-                return qualifier.singularFeminine;
-            }
-            return qualifier.pluralFeminine;
-        }
-
-        if(noun.plurality == IngredientData.Plurality.SINGULAR)
-        {
-            return qualifier.singularMasculine;
-        }
-        return qualifier.pluralMasculine;
-    }
-
-    private string MatchArticle(string article, IngredientData.Noun noun)
-    {
-        if(noun == null)
-        {
-            return "la";
-        }
-        bool isUndefined = article.StartsWith("u");
-        if(noun.plurality == IngredientData.Plurality.SINGULAR)
-        {
-            if(noun.genre == IngredientData.Genre.FEMININE)
-            {
-                if(isUndefined)
-                {
-                    return "une";
-                }
-                return noun.hasContractedArticle ? "l’" : "la";
-            }
-            if(isUndefined)
-            {
-                return "un";
-            }
-            return noun.hasContractedArticle ? "l’" : "le";
-        }
-
-        return isUndefined ? "des" : "les";
-    }
-
-    private string MatchPossessive(string article)
-    {
-        switch(article)
-        {
-            case "un" :
-                return "d’un";
-            case "une" :
-                return "d’une";
-            case "le" :
-                return "du";
-            case "la" :
-                return "de la";
-            case "les" :
-                return "des";
-            default :
-                return "de";
-        }
-    }
-
-    private string GetRandomChoice(string context)
-    {
-        string[] choices = context.Split(',');
-        return choices[Random.Range(0, choices.Length)];
+        Dictionary<string, string[]> categoryElements = TextGenerator.FetchCategories(nameData.text);
+        string template = TextGenerator.PickRandom("template", categoryElements);
+        return TextGenerator.Format(template, Ingredients);
     }
 
     private void ComputeLine()
---
+++ Assets/Scripts/TextGenerator.cs
@@ -0,0 +1,242 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TextGenerator
+{
+    public static Dictionary<string, string[]> FetchCategories(string text)
+    {
+        Dictionary<string, string[]> categoryElements = new Dictionary<string, string[]>();
+        string[] categoryNames = FetchCategory("category", text);
+        foreach(string categoryName in categoryNames)
+        {
+            categoryElements.Add(categoryName, FetchCategory(categoryName, text));
+        }
+        return categoryElements;
+    }
+
+    public static string Format(string rawString, Dictionary<IngredientData.IngredientType, IngredientInitializer> ingredients)
+    {
+
+        while(rawString.Contains("["))
+        {
+            string optionalElement = GetTextBetween("[", "]", rawString);
+            rawString = ReplaceTextBetween("[", "]", Random.value > 0.5f ? optionalElement : "", rawString);
+        }
+
+        while(rawString.Contains("("))
+        {
+            string[] options = GetTextBetween("(", ")", rawString).Split('-');
+            rawString = ReplaceTextBetween("(", ")", PickRandom(options), rawString);
+        }
+
+        List<IngredientData.Noun> matchingNouns = new List<IngredientData.Noun>();
+        matchingNouns.Add(null);
+        while(rawString.Contains("§"))
+        {
+            string placeholder = GetTextBetween("§", "§", rawString);
+
+            int matchingIndex = int.Parse(placeholder[0].ToString());
+            placeholder = placeholder.Substring(1);
+
+            IngredientData.Noun noun;
+            switch(placeholder)
+            {
+                case "planet_synonym" :
+                    noun = ingredients[IngredientData.IngredientType.SOLID].Data.NamingElements.planetSynonym;
+                    break;
+                case "concept" :
+                    noun = ingredients[IngredientData.IngredientType.SOLID].Data.NamingElements.concept;
+                    break;
+                case "first_name" :
+                    noun = ingredients[IngredientData.IngredientType.LIQUID].Data.NamingElements.firstName;
+                    break;
+                case "nickname" :
+                    noun = ingredients[IngredientData.IngredientType.SOLID].Data.NamingElements.nickname;
+                    break;
+                case "matriculation" :
+                    noun = ingredients[IngredientData.IngredientType.GASEOUS].Data.NamingElements.matriculation;
+                    break;
+                default :
+                    noun = null;
+                    break;
+            }
+            matchingNouns.Insert(matchingIndex, noun);
+
+            rawString = ReplaceTextBetween("§", "§", noun.word, rawString);
+        }
+
+        while(rawString.Contains("+"))
+        {
+            string placeholder = GetTextBetween("+", "+", rawString);
+            int matchingIndex = int.Parse(placeholder[0].ToString());
+            placeholder = placeholder.Substring(1);
+
+            IngredientData.Qualifier qualifier;
+            switch(placeholder)
+            {
+                case "style_adjective" :
+                    qualifier = ingredients[IngredientData.IngredientType.GASEOUS].Data.NamingElements.styleAdjective;
+                    break;
+                case "personality_adjective" :
+                    qualifier = ingredients[IngredientData.IngredientType.LIQUID].Data.NamingElements.personalityAdjective;
+                    break;
+                case "color" :
+                    qualifier = ingredients[IngredientData.IngredientType.LIQUID].Data.NamingElements.color;
+                    break;
+                default :
+                    qualifier = null;
+                    break;
+            }
+
+            placeholder = MatchQualifier(qualifier, matchingNouns[matchingIndex]);
+
+            rawString = ReplaceTextBetween("+", "+", placeholder, rawString);
+        }
+
+        while(rawString.Contains("<"))
+        {
+            string article = GetTextBetween("<", ">", rawString);
+            int matchingIndex = int.Parse(article[0].ToString());
+            article = article.Substring(1);
+
+            article = MatchArticle(article, matchingNouns[matchingIndex]);
+
+            rawString = ReplaceTextBetween("<", ">", article, rawString);
+        }
+
+        // rawString = HandlePunctuation(rawString);
+        rawString = rawString[0].ToString().ToUpper() + rawString.Substring(1);
+
+        return rawString;
+    }
+
+    public static string PickRandom(string categoryName, Dictionary<string, string[]> categoryElements)
+    {
+        string[] elements = categoryElements[categoryName];
+        return elements[Random.Range(0, elements.Length)];
+    }
+
+    public static string PickRandom(string[] options)
+    {
+        return options[Random.Range(0, options.Length)];
+    }
+
+    private static string[] FetchCategory(string categoryName, string context)
+    {
+        string categoryText = GetTextBetween($"#{categoryName}\n", "\n\n", context);
+        return categoryText.Split('\n');
+    }
+
+    private static string GetTextBetween(string start, string end, string context)
+    {
+        int startIndex = context.IndexOf(start) + start.Length;
+        int length = context.Substring(startIndex).IndexOf(end);
+        return context.Substring(startIndex, length);
+    }
+
+    private static string ReplaceTextBetween(string start, string end, string replacement, string context)
+    {
+        int startIndex = context.IndexOf(start);
+        int length = context.Substring(startIndex).IndexOf(end, start.Length) + end.Length;
+        string textToReplace = context.Substring(startIndex, length);
+        return context.Replace(textToReplace, replacement);
+    }
+
+    private static string MatchQualifier(IngredientData.Qualifier qualifier, IngredientData.Noun noun)
+    {
+        if(qualifier.invariable != "")
+        {
+            return qualifier.invariable;
+        }
+
+        if(noun == null)
+        {
+            return qualifier.singularFeminine;
+        }
+
+        if(noun.genre == IngredientData.Genre.FEMININE)
+        {
+            if(noun.plurality == IngredientData.Plurality.SINGULAR)
+            {
+                return qualifier.singularFeminine;
+            }
+            return qualifier.pluralFeminine;
+        }
+
+        if(noun.plurality == IngredientData.Plurality.SINGULAR)
+        {
+            return qualifier.singularMasculine;
+        }
+        return qualifier.pluralMasculine;
+    }
+
+    private static string MatchArticle(string article, IngredientData.Noun noun)
+    {
+        if(noun == null)
+        {
+            return "la";
+        }
+
+        bool isUndefined = article.Contains("u");
+        bool hasPossessive = article.Contains("p");
+        if(noun.plurality == IngredientData.Plurality.SINGULAR)
+        {
+            if(noun.genre == IngredientData.Genre.FEMININE)
+            {
+                if(isUndefined)
+                {
+                    return hasPossessive ? "d’une" : "une";
+                }
+                if(noun.hasContractedArticle)
+                {
+                    return hasPossessive ? "de l’" : "l’";
+                }
+                return hasPossessive ? "de la" : "la";
+            }
+            if(isUndefined)
+            {
+                return hasPossessive ? "d’un" : "un";
+            }
+            if(noun.hasContractedArticle)
+            {
+                return hasPossessive ? "de l’" : "l’";
+            }
+            return hasPossessive ? "du" : "le";
+        }
+        if(isUndefined)
+        {
+            return hasPossessive ? "de" : "des";
+        }
+        return hasPossessive ? "des" : "les";
+    }
+
+    private static string HandlePunctuation(string rawString)
+    {
+        char[] strongPunctuation = new char[] {'.', '!', '?'};
+        int strongPunctuationIndex = rawString.IndexOfAny(strongPunctuation);
+        bool isEndOfString = false;
+        while(strongPunctuationIndex > 0 && !isEndOfString)
+        {
+            bool isCharFound = false;
+            while(!isCharFound && !isEndOfString)
+            {
+                strongPunctuationIndex++;
+                isEndOfString = strongPunctuationIndex >= rawString.Length;
+                if(!isEndOfString && rawString[strongPunctuationIndex] != ' ')
+                {
+                    rawString =
+                            rawString.Substring(0, strongPunctuationIndex) +
+                            rawString[strongPunctuationIndex].ToString().ToUpper() +
+                            rawString.Substring(strongPunctuationIndex + 1);
+                    isCharFound = true;
+                }
+            }
+            if(!isEndOfString)
+            {
+                strongPunctuationIndex = rawString.IndexOfAny(strongPunctuation, strongPunctuationIndex);
+            }
+        }
+        return rawString;
+    }
+}
