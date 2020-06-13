6817f57c45cd93eed254cc5c605d7fc17640a299
rascar-capac <to.constant@gmail.com>
2020-06-13 16:23:47 +0200

    Enhance shootable
    Separate animation logic

---
+++ Assets/Scripts/AnimShootable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimShootable : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void SetSpeed(float speed)
+    {
+        animator.SetFloat("Speed", speed);
+    }
+
+    public void Collide()
+    {
+        animator.SetTrigger("Collide");
+    }
+
+    public void SetAiming(bool isAiming)
+    {
+        animator.SetBool("IsAiming", isAiming);
+    }
+
+    public void SetAimLevel(int aimLevel)
+    {
+        animator.SetInteger("AimLevel", aimLevel);
+    }
+}
--- Assets/Scripts/Shootable.cs
+++ Assets/Scripts/Shootable.cs
@@ -10,51 +10,26 @@ public class Shootable : MonoBehaviour
     [SerializeField] private float maxPrecisionOffset = 30f;
 
     private Camera mainCamera;
-    private Animator animator;
     private Rigidbody2D rb;
     private Vector2 mouseOffset;
+    private AnimShootable anim;
 
     private void Awake()
     {
         mainCamera = Camera.main;
-        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<AnimShootable>();
     }
-
-    // private void Start()
-    // {
-    //     aiming = false;
-    // }
-
-    // private void Update()
-    // {
-    //     if(Input.GetMouseButtonDown(0))
-    //     {
-    //         RaycastHit hit;
-    //         Ray mouseRay = mainCamera.ScreenPointToRay(Input.mousePosition);
-    //         if(Physics.Raycast(mouseRay, LayerMask.))
-    //     }
-
-    //     if(Input.GetMouseButtonUp(0))
-    //     {
-    //         Vector2 velocity = transform.position - mainCamera.ScreenToWorldPoint(Input.mousePosition);
-    //         if(velocity.magnitude > maxRadius)
-    //         {
-    //             velocity = velocity.normalized * maxRadius;
-    //         }
-    //         GetComponent<Rigidbody2D>().AddForce(velocity * power, ForceMode2D.Impulse);
-    //     }
-    // }
 
     private void Update()
     {
-        animator.SetFloat("Speed", rb.velocity.magnitude);
+        anim.SetSpeed(rb.velocity.magnitude);
     }
 
     private void OnMouseDown()
     {
-        animator.SetTrigger("Collide");
-        animator.SetBool("IsAiming", true);
+        anim?.Collide();
+        anim?.SetAiming(true);
         if(mainCamera.TryGetComponent<Dragable>(out Dragable camera))
         {
             camera.IsAiming = true;
@@ -79,12 +54,12 @@ public class Shootable : MonoBehaviour
             mouseOffset = mouseOffset.normalized * maxRadius;
             aimLevel = 2;
         }
-        animator.SetInteger("AimLevel", aimLevel);
+        anim?.SetAimLevel(aimLevel);
     }
 
     private void OnMouseUp()
     {
-        animator.SetBool("IsAiming", false);
+        anim?.SetAiming(false);
         if(mainCamera.TryGetComponent<Dragable>(out Dragable camera))
         {
             camera.IsAiming = false;
