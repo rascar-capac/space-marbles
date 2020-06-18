7344f83ac4d868cfcbcb0c83d2e01f62bcceb051
rascar-capac <to.constant@gmail.com>
2020-06-18 14:17:09 +0200

    Add a main menu and a loading screen

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] private int sceneToLoadIndex = 0;
+    [SerializeField] private GameObject loadingPanel = null;
+    [SerializeField] private Slider progressBar = null;
+
+    private void Awake()
+    {
+        if(loadingPanel)
+        {
+            loadingPanel.SetActive(false);
+        }
+    }
+
+    public void LoadLevel()
+    {
+        StartCoroutine(LoadSceneAsync(sceneToLoadIndex));
+    }
+
+    private IEnumerator LoadSceneAsync(int index)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(index);
+        if(loadingPanel)
+        {
+            loadingPanel.SetActive(true);
+        }
+
+        while(!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            if(progressBar)
+            {
+                progressBar.value = progress;
+            }
+
+            yield return null;
+        }
+    }
+}
